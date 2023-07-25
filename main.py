133a15a4b9b12ff5cd6d167867df515dabaa1e81
Nancydevs <Nancy.dubey2003@gmail.com>
2023-07-25 20:06:43 +0530

    added code to fetch from deso post

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("hello world")
+import deso 
+import json 
+
+
+
+
+desoPosts = deso.Posts()
+postsByUser = desoPosts.getPostsForPublicKey(username="bitclout_artist")
+with open("posts.json", "w") as file:
+    json.dump(postsByUser.json(), file)
